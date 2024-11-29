16eeb6ea09b909789456da57d35f91e4d6658b9e
DestinyJoyner <destinyjoyner@pursuit.org>
2024-11-29 18:04:48 -0500

    successful registration call to be with response

--- src/components/registration/RegistrationForm.jsx
+++ src/components/registration/RegistrationForm.jsx
@@ -1,9 +1,12 @@
 import {useState} from "react"
+import {useAuthProvider} from "../../providers/AuthProvider.jsx"
 import "./RegistrationForm.scss"
 
 export default function RegistrationForm () {
+const {API, axios} = useAuthProvider()
 
     const [registerForm, setRegisterForm] = useState({
+        first_name:"",
         email: "",
         password:"",
         confirm_password: "",
@@ -15,8 +18,29 @@ export default function RegistrationForm () {
        setRegisterForm({...registerForm, [id] : value}) 
     }
 
+    function handleRegisterSubmit(e) {
+        e.preventDefault()
+        axios.post(`${API}/auth/register`, {
+            login: registerForm
+        }).then(({data}) =>{
+            console.log("register response", data)
+            /* 
+            response includes : {email, first_name, id, verification_token, is_verified}
+            */
+        }).catch((err) => console.log(err))
+    }
+
      return(
-        <form className="registration_form">
+        <form className="registration_form"
+        onSubmit={(event) => handleRegisterSubmit(event)}>
+            <label>
+                <span>First Name</span>
+                <input 
+                type="text"
+                value={registerForm["first_name"]}
+                id= "first_name"
+                onChange={(event) => handleFormInput(event)} />
+            </label>
             <label>
                 <span>Email</span>
                 <input 
