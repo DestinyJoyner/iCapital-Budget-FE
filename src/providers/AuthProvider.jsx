80336ab5d208c7678f8acb680433792a3b7a6a9a
DestinyJoyner <destinyjoyner@pursuit.org>
2024-11-29 21:47:59 -0500

    updated protected route with token verification and storage/ logout implementation if invalid credentials, functioning

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -9,17 +9,19 @@ export function useAuthProvider() {
 const API = import.meta.env.VITE_API_URL
 
 export default function AuthProvider ({children}) {
+    const authToken = localStorage.getItem("token")
+    const userEmail = localStorage.getItem("icapital_user_email")
     const [userAuth, setUserAuth] = useState({
         first_name:"",
         user_id:"",
-        authToken:"",
-        email: ""
+        authToken: authToken || "",
+        email: userEmail || ""
     })
     const [verificationToken, setVerificationToken] = useState(null)
     // const [authToken, setAuthToken] = useState(null)
     // const [userId, setUserId] = useState(null)
 
-    // axios.defaults.headers.common["authorization"] = token
+    axios.defaults.headers.common["authorization"] = `Bearer ${userAuth.authToken}`
 
     return (
         <AuthData.Provider value={{
