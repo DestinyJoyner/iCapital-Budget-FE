7ec9a258f3e737ad742ea38c54d5b3ca52aa4d05
DestinyJoyner <destinyjoyner@pursuit.org>
2024-12-01 20:34:58 -0500

    added auth header and icapital logo

--- src/components/auth/AuthPage.jsx
+++ src/components/auth/AuthPage.jsx
@@ -1,13 +1,16 @@
 import LoginForm from "../login/LoginForm.jsx"
 import RegisterForm from "../registration/RegistrationForm.jsx"
+import icapitalLogo from "../../assets/icapital_logo_white.svg"
 import "./AuthPage.scss"
 
 export default function AuthPage() {
 
     return (
         <div className="authPage">
+            <section className="authPage_header flex-column-center"><img src={icapitalLogo} alt="icapital-logo" /></section>
             <section className="authPage_forms">
                 <LoginForm />
+                <hr/>
                 <RegisterForm />
             </section>
         </div>
