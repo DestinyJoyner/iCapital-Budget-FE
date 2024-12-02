1e76ee304cc1b8ace3dc8c4766c1eed99b32c1f4
DestinyJoyner <destinyjoyner@pursuit.org>
2024-12-02 00:19:24 -0500

    registration form updated with loading and error messaging to user

--- src/components/registration/RegistrationForm.jsx
+++ src/components/registration/RegistrationForm.jsx
@@ -1,96 +1,120 @@
-import {useState} from "react"
-import { useNavigate} from "react-router-dom";
-import {useAuthProvider} from "../../providers/AuthProvider.jsx"
-import "./RegistrationForm.scss"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthProvider } from "../../providers/AuthProvider.jsx";
+import Loading from "../loading/Loading.jsx";
+import "./RegistrationForm.scss";
 
-export default function RegistrationForm () {
-const {API, axios, setVerificationToken,
-    userAuth,
-    setUserAuth} = useAuthProvider()
+export default function RegistrationForm() {
+  const { API, axios, setVerificationToken, userAuth, setUserAuth } =
+    useAuthProvider();
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const [registerForm, setRegisterForm] = useState({
-        first_name:"",
-        email: "",
-        password:"",
-        confirm_password: "",
-    })
+  const [registerForm, setRegisterForm] = useState({
+    first_name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
 
-    function handleFormInput (e) {
-       const value = e.target.value
-       const id = e.target.id
-       setRegisterForm({...registerForm, [id] : value}) 
-    }
+  const [loading, setLoading] = useState(false);
+  // registration error handling
+  const [registrationError, setRegistrationError] = useState(null);
 
-    function handleRegisterSubmit(e) {
-        e.preventDefault()
-        axios.post(`${API}/auth/register`, {
-            login: registerForm
-        }).then(({data}) =>{
-            const {email, first_name, id, is_verified } = data
+  function handleFormInput(e) {
+    const value = e.target.value;
+    const id = e.target.id;
+    setRegisterForm({ ...registerForm, [id]: value });
+  }
 
-            // setVerificationToken(verification_token)
-            setUserAuth({
-                ...userAuth,
-                email,
-                first_name,
-                id,
-                is_verified
-            })
-            localStorage.setItem("icapital_user_email", email)
-            navigate("/verification")
-            /* 
+  function handleRegisterSubmit(e) {
+    e.preventDefault();
+    // trigger loading state
+    setLoading(true);
+    axios
+      .post(`${API}/auth/register`, {
+        login: registerForm,
+      })
+      .then(({ data }) => {
+        const { email, first_name, id, is_verified } = data;
+
+        // setVerificationToken(verification_token)
+        setUserAuth({
+          ...userAuth,
+          email,
+          first_name,
+          id,
+          is_verified,
+        });
+        localStorage.setItem("icapital_user_email", email);
+
+        setTimeout(() => {
+          navigate("/verification");
+          setLoading(false);
+        }, 500);
+
+        /* 
             response includes {email: 'destinytestdev@gmail.com', first_name: 'Destiny', id: 4, verification_token: '712afc709ab67db9f74113435840343243422742e20893131d59f537ff734aa8', is_verified: false, …}
             */
-           
-        }).catch((err) => console.log(err))
-    }
+      })
+      .catch((err) => {
+        console.log("registration error", err);
+        setRegistrationError(err.response?.data.error);
+        setLoading(false);
+      });
+  }
 
-     return(
-        <form className="registration_form app-card flex-column"
-        onSubmit={(event) => handleRegisterSubmit(event)}>
-            <h2>Register</h2>
-            <label>
-                
-                <input 
-                type="text"
-                value={registerForm["first_name"]}
-                id= "first_name"
-                onChange={(event) => handleFormInput(event)} />
-                <span>First Name</span>
-            </label>
-            <label>
-                
-                <input 
-                type="text"
-                value={registerForm["email"]}
-                id= "email"
-                onChange={(event) => handleFormInput(event)} />
-                <span>Email</span>
-            </label>
+  return (
+    <form
+      className="registration_form app-card flex-column"
+      onSubmit={(event) => handleRegisterSubmit(event)}
+    >
+      <h2>Register</h2>
+      <label>
+        <input
+          type="text"
+          value={registerForm["first_name"]}
+          id="first_name"
+          onChange={(event) => handleFormInput(event)}
+        />
+        <span>First Name</span>
+      </label>
+      <label>
+        <input
+          type="text"
+          value={registerForm["email"]}
+          id="email"
+          onChange={(event) => handleFormInput(event)}
+        />
+        <span>Email</span>
+      </label>
 
-            <label>
-                
-                <input 
-                type="password"
-                value={registerForm["password"]}
-                id= "password"
-                onChange={(event) => handleFormInput(event)}  />
-                <span>Password</span>
-            </label>
+      <label>
+        <input
+          type="password"
+          value={registerForm["password"]}
+          id="password"
+          onChange={(event) => handleFormInput(event)}
+        />
+        <span>Password</span>
+      </label>
 
-            <label>
-                
-                <input 
-                type="password" 
-                value={registerForm["confirm_password"]}
-                id= "confirm_password"
-                onChange={(event) => handleFormInput(event)}/>
-                <span>Confirm Password</span>
-            </label>
+      <label>
+        <input
+          type="password"
+          value={registerForm["confirm_password"]}
+          id="confirm_password"
+          onChange={(event) => handleFormInput(event)}
+        />
+        <span>Confirm Password</span>
+      </label>
 
-            <input type="submit" value="Register" />
-        </form>
-     )
+      <input type="submit" value="Register" disabled={loading} />
+
+      {loading && <Loading />}
+      {registrationError && (
+        <span className="registration_form_error">{registrationError}</span>
+      )}
+    </form>
+  );
 }
