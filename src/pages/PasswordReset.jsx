522eecebf52564c1f5d612a3118abd2b00c6726b
DestinyJoyner <destinyjoyner@pursuit.org>
2024-12-03 02:13:25 -0500

    fixed put request error in be fro password update, loading state added and delay for state update to login, full forgot password functionality working

--- src/pages/PasswordReset.jsx
+++ src/pages/PasswordReset.jsx
@@ -1,15 +1,17 @@
 import {useEffect, useState} from "react"
-import {useParams, Navigate} from "react-router-dom"
+import {useParams, Navigate, useNavigate} from "react-router-dom"
 import {useAuthProvider} from "../providers/AuthProvider.jsx"
 import {handleFormTextInput} from "../utils/authFormFunctions.js"
+import Loading from "../components/loading/Loading.jsx"
 
 export default function PasswordReset () {
     // page linked from email, with verfication code in url -> useParams() to send to BE password controller in useEffect, when validated, reveal new password form on submission, send to be for PUT request
 
     // no verificationToken, means just linked here to initiate password rest, user submits email for verification, then receives email, then linked back here w/ token to set new password
 
-    const {API, axios} = useAuthProvider()
+    const {API, axios, setUserAuth, userAuth} = useAuthProvider()
     const {verificationToken} = useParams()
+    const navigate = useNavigate()
 
     const [userEmail, setUserEmail] = useState("")
     const [resetLinkSent, setResetLink] = useState(false)
@@ -21,13 +23,53 @@ export default function PasswordReset () {
     })
 
     const [passwordMatch, setPasswordMatch] = useState(false)
+    const [loading, setLoading] = useState(false)
+
 
     function handleEmailSubmit (e) {
         e.preventDefault()
-        axios.post(`${API}/auth/password`, {email:userEmail}).then(({data}) => setResetLink(true)).catch(err => console.log("password reset request failed,", err))
+        setLoading(true)
+        axios.post(`${API}/auth/password`, {email:userEmail}).then(({data}) => {
+            setResetLink(true) 
+            setLoading(false)
+    }).catch(err => {
+        setLoading(false)
+        console.log("password reset request failed,", err)})
     }
 
     // submission on new passwords, send verfication token in body
+
+    function handlePasswordSubmit(e) {
+        e.preventDefault()
+        setLoading(true)
+        // axios.put(`${API}/auth/password/reset`).then(({data}) => console.log(data)).catch(err => console.log("not reading route"))
+
+
+        axios.put(`${API}/auth/password/reset`, {
+            password: password["new_password"],
+            verificationToken : verificationToken
+        }).then(({data}) => {
+            // console.log("password reset res",data)
+            const {token, email} = data
+            localStorage.setItem("token", token)
+            localStorage.setItem("icapital_user_email", email )
+            // update user auth as its checked in protected route for dashboard
+            setUserAuth({
+                ...userAuth,
+                authToken: token,
+                email: email
+            })
+
+            // delay to wait for state update
+            setTimeout(() => {
+                setLoading(false)
+                navigate("/dashboard");
+            }, 500);
+           
+        }).catch(err => {
+            setLoading(false)
+            console.log("password change error", err)})
+    }
 
     // verify token in url
     useEffect(() => {
@@ -59,14 +101,19 @@ export default function PasswordReset () {
                 value = {userEmail}
                 onChange={(e) => setUserEmail(e.target.value)} />
             </label>
-            <input type="submit" />
+            <input type="submit" disabled ={loading} />
+            {loading && <Loading />}
         </form> 
-        :
+        :  
+        <>
+        
         <h2>Check Email for Password Reset Link</h2>
+        </>
+        
     
         :
         verificationToken && tokenVerified ?
-        <form>
+        <form onSubmit ={(e) =>handlePasswordSubmit(e)}>
             <h2>Create New Password</h2>
             <label>
                 <span>New Password</span>
@@ -87,8 +134,9 @@ export default function PasswordReset () {
             />
             </label>
             <span>{passwordMatch ? "Passwords Match" : "Passwords don't match"}</span>
-           { passwordMatch && <input type="submit" 
+           { passwordMatch && <input type="submit" disabled={loading}
              />}
+            {loading && <Loading />}
         </form>
         :
         // <Navigate to="/auth" />
