9fb6ef941964a6b34386f0f69f46613306059b37
DestinyJoyner <destinyjoyner@pursuit.org>
2024-12-05 06:18:14 -0500

    added key to transactions mapping

--- src/components/transactions/Transactions.jsx
+++ src/components/transactions/Transactions.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TransactionCard from "./TransactionCard.jsx";
+import { v4 as uuidv4 } from "uuid";
 import "./Transactions.scss";
 
 export default function Transactions({ userTransactions }) {
@@ -7,7 +8,7 @@ export default function Transactions({ userTransactions }) {
     <section className="transactions app-card">
       <h2>Transactions</h2>
       {userTransactions.map((transaction) => (
-        <TransactionCard transaction={transaction} />
+        <TransactionCard transaction={transaction} key={uuidv4()} />
       ))}
     </section>
   );
