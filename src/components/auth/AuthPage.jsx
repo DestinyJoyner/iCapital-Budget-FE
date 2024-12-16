48718a8d2e3d1a6053003cb40f9c29a377aee72e
DestinyJoyner <destinyjoyner@pursuit.org>
2024-12-16 11:05:29 -0500

    added disclaimer to homepage

--- src/components/auth/AuthPage.jsx
+++ src/components/auth/AuthPage.jsx
@@ -8,6 +8,9 @@ export default function AuthPage() {
     return (
         <div className="authPage">
             <AppHeader />
+            <span className="subtext-font authPage_disclaimer">
+                        Note: Due to backend hosting services, initial login after periods of inactivity may take 30-45 seconds to process.
+                    </span>
             <section className="authPage_forms">
                 <LoginForm />
                 <hr/>
